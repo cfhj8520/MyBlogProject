1e9fe4075163f9847ca10ccb46153260566e811d
cfhj8520 <cfhj8520@naver.com>
2021-11-20 16:39:31 +0900

    테스트 케이스 생성

---
+++ src/test/java/com/fastcampus/UserRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.fastcampus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fastcampus.domain.Role;
+import com.fastcampus.domain.User;
+import com.fastcampus.persistence.UserRepository;
+
+@SpringBootTest
+class UserRepositoryTests {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	void testGetUser() {	
+		User user = new User();
+		user.setUsername("test");
+		user.setPassword("test123");
+		user.setRole(Role.ADMIN);
+		user.setName("둘리");
+		userRepository.save(user);
+
+		assertNotNull(userRepository.findById("test"));
+	}
+
+}
