7b5bc4bd076a231aef702cfc17a57d36ea2d0461
cfhj8520 <cfhj8520@naver.com>
2021-11-22 20:24:02 +0900

    usercontroller 생성

---
+++ src/main/java/com/fastcampus/jblog/controller/user/UserController.java
@@ -0,0 +1,34 @@
+package com.fastcampus.jblog.controller.user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fastcampus.jblog.biz.user.UserService;
+import com.fastcampus.jblog.biz.user.UserVO;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/loginView")
+	public String loginView() {
+		return "bloglogin";
+	}
+	
+	@RequestMapping("/login")
+	public String login(UserVO vo, HttpSession session) {
+		UserVO user = userService.getUser(vo);
+		if(user != null) {
+			session.setAttribute("user", user);
+			
+			
+		}
+		
+		return "redirect:/";
+	}
+}
