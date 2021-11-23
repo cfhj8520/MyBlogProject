7a6d0f0edebb6169033c2b863a4726eb193be179
cfhj8520 <cfhj8520@naver.com>
2021-11-23 21:51:12 +0900

    수정 : 검색을 위한 쿼리문 작성

--- src/main/java/com/fastcampus/jblog/biz/blog/BlogDAO.java
+++ src/main/java/com/fastcampus/jblog/biz/blog/BlogDAO.java
@@ -24,12 +24,17 @@ public class BlogDAO {
 	private String BLOG_UPDATE = "update blog set title = ?, tag = ?, cnt_display_post = ? where blog_id = ?";
 	private String BLOG_DELETE = "delete blog where blog_id = ?";
 	
+	private String BLOG_LIST_TITLE   	= "select b.blog_id, b.cnt_display_post, b.status, b.title, b.tag, u.user_name from blog b, blog_user u where b.user_id=u.user_id and b.title like '%'||?||'%' order by b.title";
+	private String BLOG_LIST_TAG     	= "select b.blog_id, b.cnt_display_post, b.status, b.title, b.tag, u.user_name from blog b, blog_user u where b.user_id=u.user_id and b.tag like '%'||?||'%' order by b.title";
+	private String BLOG_LIST_BLOGGER 	= "select b.blog_id, b.cnt_display_post, b.status, b.title, b.tag, u.user_name from blog b, blog_user u where b.user_id=u.user_id and u.user_name like '%'||?||'%' order by b.title";
+
+	
 	public void insertBlog(BlogVO vo) {
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BLOG_INSERT);
 			stmt.setString(1, vo.getTitle());
-			stmt.setString(2, "미등록");
+			stmt.setString(2, "No Tags");
 			stmt.setInt(3, 3);
 			stmt.setString(4, "운영");
 			stmt.setInt(5, vo.getUser_id());
@@ -109,7 +114,14 @@ public class BlogDAO {
 		List<BlogVO> blogList = new ArrayList<BlogVO>();
 		try {
 			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BLOG_LIST);
+			if(vo.getSearchCondition().equals("TITLE")) {
+				stmt = conn.prepareStatement(BLOG_LIST_TITLE);
+			}else if(vo.getSearchCondition().equals("TAG")) {
+				stmt = conn.prepareStatement(BLOG_LIST_TAG);
+			}else if(vo.getSearchCondition().equals("BLOGGER")) {
+				stmt = conn.prepareStatement(BLOG_LIST_BLOGGER);
+			}
+			stmt.setString(1, "");
 			rs = stmt.executeQuery();
 			while(rs.next()) {
 				BlogVO blog = new BlogVO();
@@ -118,7 +130,7 @@ public class BlogDAO {
 				blog.setTag(rs.getString("TAG"));
 				blog.setCnt_display_post(rs.getInt("CNT_DISPLAY_POST"));
 				blog.setStatus(rs.getString("STATUS"));
-				blog.setUser_id(rs.getInt("USER_ID"));
+				blog.setUser_name(rs.getString("USER_NAME"));
 				blogList.add(blog);
 			}
 		} catch (Exception e){
