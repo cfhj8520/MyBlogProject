4e0951d27bb60fe175449e6124427faf8264cea0
cfhj8520 <cfhj8520@naver.com>
2021-11-23 21:50:31 +0900

    수정 : 변수명 규칙 통일

--- src/main/java/com/fastcampus/jblog/biz/user/UserDAO.java
+++ src/main/java/com/fastcampus/jblog/biz/user/UserDAO.java
@@ -29,7 +29,7 @@ public class UserDAO{
 				user.setUser_id(rs.getInt("USER_ID"));
 				user.setId(rs.getString("ID"));
 				user.setPassword(rs.getString("PASSWORD"));
-				user.setUserName(rs.getString("USER_NAME"));
+				user.setUser_name(rs.getString("USER_NAME"));
 				user.setRole(rs.getString("ROLE"));
 			}
 		} catch (Exception e) {
--- src/main/java/com/fastcampus/jblog/biz/user/UserVO.java
+++ src/main/java/com/fastcampus/jblog/biz/user/UserVO.java
@@ -7,6 +7,6 @@ public class UserVO {
 	private int user_id;			//회원 일련번호
 	private String id;			//회원 아이디
 	private String password;
-	private String userName;
+	private String user_name;
 	private String role;
 }
