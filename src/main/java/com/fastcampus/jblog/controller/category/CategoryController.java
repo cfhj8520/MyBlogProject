b15207d454843b30ae9fef021bb80cd9beb7ee7d
cfhj8520 <cfhj8520@naver.com>
2021-11-25 20:54:20 +0900

    추가 : 게시글 확인 시 매개변수 제거 및 createCategory, getCategoryList 매핑 추가

--- src/main/java/com/fastcampus/jblog/controller/category/CategoryController.java
+++ src/main/java/com/fastcampus/jblog/controller/category/CategoryController.java
@@ -1,12 +1,17 @@
 package com.fastcampus.jblog.controller.category;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fastcampus.jblog.biz.blog.BlogService;
+import com.fastcampus.jblog.biz.blog.BlogVO;
 import com.fastcampus.jblog.biz.category.CategoryService;
 import com.fastcampus.jblog.biz.category.CategoryVO;
+import com.fastcampus.jblog.biz.user.UserVO;
 
 @Controller
 public class CategoryController {
@@ -14,11 +19,30 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 	
+	@Autowired
+	private BlogService blogService;
+	
 	@RequestMapping("/getCategoryList")
-	public String getCategoryList(CategoryVO vo, Model model) {
-		model.addAttribute("categoryList", categoryService.getCategoryList(vo));
+	public String getCategoryList(HttpSession session, Model model) {
+		UserVO user = (UserVO) session.getAttribute("user");
+		BlogVO blog = new BlogVO();
+		CategoryVO category = new CategoryVO();
+		
+		category.setBlog_id(user.getUser_id());
+		blog.setUser_id(user.getUser_id());
+		
+		model.addAttribute("categoryList", categoryService.getCategoryList(category));
+		model.addAttribute("blog", blogService.getBlog(blog));
 		
 		return "blogadmin_category";
 	}
 	
+	@RequestMapping("/createCategory")
+	public String createCategory(CategoryVO vo, HttpSession session) {
+		UserVO user = (UserVO) session.getAttribute("user");
+		vo.setBlog_id(user.getUser_id());
+		categoryService.insertCategory(vo);
+		
+		return "redirect:/getCategoryList";
+	}
 }
