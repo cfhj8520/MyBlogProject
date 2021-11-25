1c9f65854958e0d634638105398822ae49998451
cfhj8520 <cfhj8520@naver.com>
2021-11-25 15:08:21 +0900

    수정 : blogInsert에서 blog_id와 user_id가 달라지는 오류 수정

--- src/main/java/com/fastcampus/jblog/biz/blog/BlogDAO.java
+++ src/main/java/com/fastcampus/jblog/biz/blog/BlogDAO.java
@@ -19,7 +19,7 @@ public class BlogDAO {
 	
 	private String BLOG_GET = "select * from blog where user_id = ?";
 	private String BLOG_INSERT = "insert into blog(blog_id, title, tag, cnt_display_post, status, user_id)" + 
-								 "values((select nvl(max(blog_id), 0) + 1 from blog), ?, ?, ?, ?, ?)";
+								 "values(?, ?, ?, ?, ?, ?)";
 	private String BLOG_UPDATE = "update blog set title = ?, tag = ?, cnt_display_post = ?, status = ? where blog_id = ?";
 	private String BLOG_DELETE = "delete blog where blog_id = ?";
 	
@@ -32,11 +32,12 @@ public class BlogDAO {
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BLOG_INSERT);
-			stmt.setString(1, vo.getTitle());
-			stmt.setString(2, "No Tags");
-			stmt.setInt(3, 3);
-			stmt.setString(4, "운영");
-			stmt.setInt(5, vo.getUser_id());
+			stmt.setInt(1, vo.getUser_id());
+			stmt.setString(2, vo.getTitle());
+			stmt.setString(3, "No Tags");
+			stmt.setInt(4, 3);
+			stmt.setString(5, "운영");
+			stmt.setInt(6, vo.getUser_id());
 			stmt.executeUpdate();
 			
 		}catch (Exception e){
