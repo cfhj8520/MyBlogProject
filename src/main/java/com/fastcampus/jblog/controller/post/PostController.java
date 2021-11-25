ce2a39bf0f29a1793ef010461a32d450ebe686e5
cfhj8520 <cfhj8520@naver.com>
2021-11-25 23:43:19 +0900

    추가 : createPost() 블로그 model 추가

--- src/main/java/com/fastcampus/jblog/controller/post/PostController.java
+++ src/main/java/com/fastcampus/jblog/controller/post/PostController.java
@@ -1,10 +1,16 @@
 package com.fastcampus.jblog.controller.post;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fastcampus.jblog.biz.blog.BlogService;
+import com.fastcampus.jblog.biz.blog.BlogVO;
 import com.fastcampus.jblog.biz.post.PostService;
+import com.fastcampus.jblog.biz.user.UserVO;
 
 @Controller
 public class PostController {
@@ -12,8 +18,18 @@ public class PostController {
 	@Autowired
 	private PostService postService;
 	
+	@Autowired
+	private BlogService blogService;
+	
 	@RequestMapping("createPost")
-	public String createPost() {
+	public String createPost(HttpSession session, Model model) {
+		UserVO user = (UserVO) session.getAttribute("user");
+		BlogVO blog = new BlogVO();
+		
+		blog.setUser_id(user.getUser_id());
+		
+		model.addAttribute("blog", blogService.getBlog(blog));
+		
 		return "adminPost";
 	}
 }
