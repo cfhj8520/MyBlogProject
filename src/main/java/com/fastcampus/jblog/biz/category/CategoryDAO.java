da0dcdca955124c73c6bc549f2c073819ee5b0ae
cfhj8520 <cfhj8520@naver.com>
2021-11-25 17:39:22 +0900

    생성 : categoryDAO 생성

--- src/main/java/com/fastcampus/jblog/biz/category/CategoryDAO.java
+++ src/main/java/com/fastcampus/jblog/biz/category/CategoryDAO.java
@@ -3,6 +3,9 @@ package com.fastcampus.jblog.biz.category;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -15,16 +18,23 @@ public class CategoryDAO {
 	private PreparedStatement stmt;
 	private ResultSet rs;
 	
-	private String CATEGORY_INSERT = "";
-	private String CATEGORY_UPDATE = "";
-	private String CATEGORY_DELETE = "";
-	private String CATEGORY_GET = "";
-	private String CATEGORY_GETLIST = "";
+	private String CATEGORY_INSERT = "insert into category(blog_id, category_id, category_name, display_type, cnt_display_post, description, created_date) "
+								   + "values(?, (select nvl(max(category_id), 0) + 1 from category), ?, ?, ?, ? , ?)";
+	private String CATEGORY_UPDATE = "update category set category_name = ?, display_type = ?, cnt_display_post = ?, description = ?, modified_date = ? where category_id = ?";
+	private String CATEGORY_DELETE = "delete from category where category_id = ?";
+	private String CATEGORY_GET = "select * from category where category_id = ?";
+	private String CATEGORY_GETLIST = "select * from category";
 	
 	public void insertCategory(CategoryVO vo) {
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(CATEGORY_INSERT);
+			stmt.setInt(1, vo.getBlog_id());
+			stmt.setString(2, vo.getCategory_name());
+			stmt.setString(3, vo.getDisplay_type());
+			stmt.setInt(4, vo.getCnt_display_post());
+			stmt.setString(5, vo.getDescription());
+			stmt.setDate(6, (java.sql.Date) vo.getCreated_date());
 			stmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -36,7 +46,13 @@ public class CategoryDAO {
 	public void updateCategory(CategoryVO vo) {
 		try {
 			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(CATEGORY_INSERT);
+			stmt = conn.prepareStatement(CATEGORY_UPDATE);
+			stmt.setString(1, vo.getCategory_name());
+			stmt.setString(2, vo.getDisplay_type());
+			stmt.setInt(3, vo.getCnt_display_post());
+			stmt.setString(4, vo.getDescription());
+			stmt.setDate(5, (java.sql.Date) new Date());
+			stmt.setInt(6, vo.getCategory_id());
 			stmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -48,7 +64,8 @@ public class CategoryDAO {
 	public void deleteCategory(CategoryVO vo) {
 		try {
 			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(CATEGORY_INSERT);
+			stmt = conn.prepareStatement(CATEGORY_DELETE);
+			stmt.setInt(1, vo.getCategory_id());
 			stmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -57,5 +74,56 @@ public class CategoryDAO {
 		}
 	}
 	
+	public CategoryVO getCategory(CategoryVO vo) {
+		CategoryVO category = null;
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(CATEGORY_GET);
+			stmt.setInt(1, vo.getCategory_id());
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				category = new CategoryVO();
+				category.setBlog_id(rs.getInt("BLOG_ID"));
+				category.setCategory_id(rs.getInt("CATEGORY_ID"));
+				category.setCategory_name(rs.getString("CATEGORY_NAME"));
+				category.setDisplay_type(rs.getString("DISPLAY_TYPE"));
+				category.setCnt_display_post(rs.getInt("CNT_DISPLAY_POST"));
+				category.setDescription(rs.getString("DESCRIPTION"));
+				category.setCreated_date(rs.getDate("CREATED_DATE"));
+				category.setModified_date(rs.getDate("MODIFIED_DATE"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return category;
+	}
+	
+	public List<CategoryVO> getCategoryList(CategoryVO vo) {
+		List<CategoryVO> categoryList = new ArrayList<CategoryVO>();
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(CATEGORY_GETLIST);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				CategoryVO category = new CategoryVO();
+				category.setBlog_id(rs.getInt("BLOG_ID"));
+				category.setCategory_id(rs.getInt("CATEGORY_ID"));
+				category.setCategory_name(rs.getString("CATEGORY_NAME"));
+				category.setDisplay_type(rs.getString("DISPLAY_TYPE"));
+				category.setCnt_display_post(rs.getInt("CNT_DISPLAY_POST"));
+				category.setDescription(rs.getString("DESCRIPTION"));
+				category.setCreated_date(rs.getDate("CREATED_DATE"));
+				category.setModified_date(rs.getDate("MODIFIED_DATE"));
+				categoryList.add(category);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return categoryList;
+	}
 	
 }
